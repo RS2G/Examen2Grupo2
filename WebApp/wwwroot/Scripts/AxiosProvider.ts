db33c0a6728360acff834dca8f115f78d51574f4
Andres Rodriguez Morales <a-ndresrm@hotmail.com>
2022-04-15 14:16:00 -0600

    Andres

--- WebApp/wwwroot/Scripts/AxiosProvider.ts
+++ WebApp/wwwroot/Scripts/AxiosProvider.ts
@@ -1,7 +1,8 @@
 ﻿
 namespace App.AxiosProvider   {
 
-    //export const GuardarEmpleado = () => axios.get<Entity.DBEntity>("aplicacion").then(({data})=>data );
+    export const ClienteEliminar = (id) => axios.delete<DBEntity>("Cliente/Grid?handler=Eliminar&id=" + id).then(({ data }) => data);
+    export const ClienteGuardar = (entity) => axios.post<DBEntity>("Cliente/Edit", entity).then(({ data }) => data);
 
 
 }
