953038a09fbe000a565e18c0ecb9b54a4bf9b01a
ABIMAEL MESEN <amesenc0498@uvirtual.ac.cr>
2022-04-16 12:22:47 -0600

    Nuevo LayoutTemplate

--- WebApp/App_Start/ContainerExtensions.cs
+++ WebApp/App_Start/ContainerExtensions.cs
@@ -16,6 +16,8 @@ namespace WebApp
             services.AddSingleton<IDataAccess, DataAccess>();
             services.AddTransient<ISolicitudService, SolicitudService>();
             services.AddTransient<IServicioService, ServicioService>();
+            services.AddTransient<IClienteService, ClienteService>();
+
             
             return services;
         }
